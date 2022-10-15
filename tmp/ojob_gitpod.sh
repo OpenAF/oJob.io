b12e7c5776d4ccc4a9fc85e4aef78819df2d3614
Nuno Aguiar <nmaguiar@users.noreply.github.com>
2022-10-15 04:56:43 +0000

    update help configPL

--- tmp/ojob_gitpod.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-nohup /home/openvscode-server/.server0.sh > /tmp/server0.log 2>&1 &
-cd /home/workspace
-exec ${OPENVSCODE_SERVER_ROOT}/bin/openvscode-server --host 0.0.0.0 --without-connection-token -- $*
--- tmp/ojob_gitpod0.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-nohup sudo dockerd > ~/.dockerd.log 2>&1 &
-/home/.openvscode-server/bin/remote-cli/openvscode-server --install-extension GitHub.github-vscode-theme 
