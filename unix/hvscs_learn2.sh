b795e5e4dd2e578523388d568809e6438f4ebffb
Nuno Aguiar <nmaguiar@users.noreply.github.com>
2022-10-24 04:02:28 +0000

    +hvscs2

---
+++ unix/hvscs_learn2.sh
@@ -0,0 +1,119 @@
+#!/bin/bash
+
+WEB_PORT=${_WEB_PORT:-8443}
+SSH_PORT=${_SSH_PORT:-22222}
+SOCKS_PORT=${_SOCKS_PORT:-10080}
+NAME=${_NAME:-hvscs}
+WORKSPACE=${_WORKSPACE:-}
+IMAGE=${_IMAGE:-nmaguiar/hvscs}
+SSH_PASS=${_SSH_PASS:-Password1}
+HOST=${_HOST:-127.0.0.1}
+
+# -----------------
+
+NOSSH=${_NOSSH:-}
+NODOCKER=${_NODOCKER:-}
+NOK8S=${_NOK8S:-}
+NOVSCODE=${_NOVSCODE:-}
+NOPULL=${_NOPULL:-}
+
+ORIG=$1
+echo "hVSCs (https://github.com/nmaguiar/hvscs)"
+echo =========================================
+
+_printHelp() {
+  echo
+  echo Please use "hvscs start" or "hvscs stop"
+  echo
+  exit
+}
+
+_start() {
+  MAC=false
+  if [ ! "`uname -s`" != "Darwin" ]; then
+    MAC=true
+  fi
+
+  echo -- Creating hVSCs network...
+  docker network create $NAME
+
+  echo "-- Starting hVSCs server (ssh port $SSH_PORT)..."
+  CMD=""
+  if [ "$MAC" != "false" ]; then
+    #CMD="--cgroupns=host -v /sys/fs/cgroup:/sys/fs/cgroup:rw"
+    CMD="--cgroupns=host"
+  fi
+  if [ -z $WORKSPACE ]; then
+    WKS=""
+  else
+    WKS="-v $WORKSPACE:/workspace:cached"
+  fi
+  EXTRA=""
+  EXTRA=$EXTRA${NOSSH:+" --env NOSSH=1 "}
+  EXTRA=$EXTRA${NODOCKER:+" --env NODOCKER=1 "}
+  EXTRA=$EXTRA${NOK8S:+" --env NOK8S=1 "}
+  EXTRA=$EXTRA${NOVSCODE:+" --env NOVSCODE=1 "}
+
+  if [ ! -z $NOPULL ]; then
+    docker pull $IMAGE
+  fi
+  docker run --rm -ti --env SSH_PASS=$SSH_PASS $EXTRA --init -d -p 3000 -p $WEB_PORT:8443 -p $SOCKS_PORT:1080 -p $SSH_PORT:22 --privileged $CMD $WKS --network $NAME --name $NAME\_hvscs $IMAGE
+
+  if [ -z $NOVSCODE ]; then
+    echo
+    echo -- Waiting for startup...
+    curl --version 2>&1 > /dev/null
+    if [ $? != 0 ]; then
+      echo -- 'curl' not installed. Please open the browser with the provided address and refresh until a web page shows up indicating the server is up.
+      echo
+    else
+      until curl -s -f -k -o /dev/null https://127.0.0.1:$WEB_PORT
+      do
+        sleep 3
+      done
+      echo =============================================================
+      echo READY!
+    fi
+
+    echo =============================================================
+    echo Open your browser at:
+    echo
+    echo " --> [ https://$HOST:$WEB_PORT/?folder=/workspace ] <--"
+    echo
+    echo =============================================================
+    echo
+    echo "To stop run 'hvscs2 stop'"
+    echo
+  fi
+}
+
+_stop() {
+  echo -- Stopping hVSCs...
+  docker stop $NAME\_hvscs
+  docker rm $NAME\_hvscs 2> /dev/null
+
+  echo -- Deleting network...
+  docker network rm $NAME
+
+  echo Done.
+}
+
+if [ -z $1 ]; then
+  START=${_OP:-start}
+else
+  START=$1
+fi
+
+case $START in
+  start)
+    _start
+    ;;
+
+  stop)
+    _stop
+    ;;
+
+  *)
+    _printHelp
+    ;;
+esac
---
+++ win/hvscs_learn2.bat
@@ -0,0 +1,64 @@
+@echo off
+
+set WEB_PORT=8443
+set SSH_PORT=22222
+set SOCKS_PORT=10080
+set NAME=hvscs_learn
+set WORKSPACE=
+set IMAGE=nmaguiar/hvscs
+set SSH_PASS=Password1
+
+rem ----------------
+
+set CMD="%1"
+if defined CMD (
+  if "%CMD%"==""start"" (
+    goto _start 
+  ) else (
+    if "%CMD%"==""stop""  ( 
+      goto _stop 
+    ) else (
+      if "%CMD%"=="""" (
+        goto _start
+      ) else (
+        echo.
+        echo -- Please use "hvscs start" or "hvscs stop"
+        echo.
+        goto _end
+      ) 
+    )
+  )
+) else (
+  goto _start
+)
+
+
+:_start
+
+echo -- Creating hVSCs network...
+docker network create %NAME%
+
+echo -- Starting hVSCs server (ssh port %SSH_PORT%)...
+docker pull %IMAGE%
+if defined WORKSPACE (
+  set WKS=-v %WORKSPACE%:/workspace:cached
+) else (
+  set WKS= 
+)
+docker run --rm -ti --init --env SSH_PASS=%SSH_PASS% -d -p 3000 -p $WEB_PORT%:8443 -p %SOCKS_PORT%:1080 -p %SSH_PORT%:22 --privileged %WKS% --network %NAME% --name %NAME%_hvscs %IMAGE%
+
+echo.
+echo -- Try to access https://127.0.0.1:%WEB_PORT%/?folder=/workspace in a couple of seconds (keep refreshing until you see a web page showing up)...
+echo -- To end run: "hvscs.bat stop"
+echo.
+goto _end
+
+:_stop
+
+echo -- Stopping hVSCs...
+docker stop %NAME%_hvscs
+echo -- Deleting network...
+docker network rm %NAME%
+echo -- Done.
+
+:_end
